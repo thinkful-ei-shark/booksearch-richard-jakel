5af93f22e587304eb496b458dea9eff146156046
Aelof3 <folea3@gmail.com>
2020-10-05 15:17:07 -0400

    upload build for gh-pages

---
+++ precache-manifest.7930c47f907a15b83db3d0d61592607e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b378bf46db550d77efe3a8949dfcf0fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7eb331aac59949b09bd",
+    "url": "/static/css/main.ec1c9c3d.chunk.css"
+  },
+  {
+    "revision": "256c30778aa9e55c452f",
+    "url": "/static/js/2.c1f8b2ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1f8b2ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7eb331aac59949b09bd",
+    "url": "/static/js/main.4778b4aa.chunk.js"
+  },
+  {
+    "revision": "b4279166c6a1c8e71e43",
+    "url": "/static/js/runtime-main.ede42aad.js"
+  }
+]);
